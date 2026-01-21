b5ab41a2b3beaa56dda6609df699583c18aed9e5
Rayannemerz <rayanne.merzouki@insa-lyon.fr>
2026-01-21 19:13:59 +0100

    Update 3config.py

--- 3config.py
+++ 3config.py
@@ -1,25 +1,25 @@
 import json
-import os
-import re
+import os #créer des dossiers et manipuler les chemins de fichiers
+import re #recherches rapide
 
 def genere_config():
-    intent_file='intent_file.json'
-    output_dir='configs'
+    intent_file='intent_file.json' #fichier source
+    output_dir='configs' #dossier contenant les fichiers .cfg
     try:
         with open(intent_file,'r') as f:
-            data=json.load(f)
+            data=json.load(f)  #lis et converti en dico
     except FileNotFoundError:
         print(f"Erreur : Le fichier '{intent_file}' est introuvable.")
         return
     except json.JSONDecodeError:
         print(f"Erreur : Impossible de lire le fichier '{intent_file}'. Vérifiez la syntaxe JSON. ")
         return
-    protocoles_dict = {p['nom'].lower(): p for p in data.get('protocoles',[])}
-    if not os.path.exists(output_dir):
+    protocoles_dict = {p['nom'].lower(): p for p in data.get('protocoles',[])} #récupération des protocoles sous forme de dico/get en cas d'oubli crée un dico vide
+    if not os.path.exists(output_dir):  #cree le dossier s'il n'existe pas
         os.makedirs(output_dir)
-    for r in data['routeurs']:
-        asn=r['asn']
-        hostname=r['hostname']
+    for r in data['routeurs']: #pour chaque routeur
+        asn=r['asn'] #prend le num de l'as
+        hostname=r['hostname'] #et son nom
         lines=[]
         lines.append("!")
         lines.append("\n")
@@ -31,15 +31,15 @@ def genere_config():
         lines.append(f"hostname {hostname}")
         lines.append("ipv6 unicast-routing")
         lines.append("!")
-        protocoles_a_activer= set()
+        protocoles_a_activer= set() #création d'un set vide != liste pour ne pas avoir de doublons active_protocols
         for interface in r['interfaces']:
             lines.append(f"interface {interface['name']}")
             lines.append(f" ipv6 address {interface['ipv6']}")
             lines.append(" ipv6 enable")
             lines.append(" no shutdown")
-            protocoles_actifs=interface.get('protocole',[])
+            protocoles_actifs=interface.get('protocole',[]) #liste des proto sur cette interface
             for proto in protocoles_actifs:
-                protocoles_a_activer.add(proto)
+                protocoles_a_activer.add(proto) #on ajt ce proto a configurer
                 if proto.lower() == 'rip':
                     lines.append(f" ipv6 rip rip{asn} enable")
                 elif proto.lower() == 'ospf':
@@ -47,28 +47,28 @@ def genere_config():
 
             lines.append( "exit")
             lines.append("!")
-        for proto  in protocoles_a_activer:
-            if proto.lower() in protocoles_dict:
+        for proto  in protocoles_a_activer: #config le protocole avec le set une fois par routeru
+            if proto.lower() in protocoles_dict: #converti en minuscule lower
                 p = protocoles_dict[proto.lower()]
-                if p.get('nom').upper() == 'RIP':
+                if p.get('nom').upper() == 'RIP': #upper majuscule
                     lines.append("ipv6 router rip rip1")
                     if p.get('parametres',{}).get('redistribution'):
                         lines.append(" redistribute connected")
                     lines.append(" exit")
                 elif p.get('nom').upper() == 'OSPF':
                     lines.append(f"ipv6 router ospf {asn}")
-                    digits = re.findall(r'\d+', hostname)
+                    digits = re.findall(r'\d+', hostname) #re.findall cherche partout dans le txt
                     if digits:
                         rid=digits[0]
                         lines.append(f" router-id {rid}.{rid}.{rid}.{rid}")
-                    else:
+                    else:# on donne un id par defaut si le routeur n'a pas de chiffre
                         lines.append(" router-id 1.1.1.1")
                     lines.append(" exit")
                 lines.append("!")
-        if 'bgp_neighbors' in r and r['bgp_neighbors']:
-            bgp_neighbors = r['bgp_neighbors']
-            asn = r['asn']
-            lines.append(f"router bgp {asn}")
+        if 'bgp_neighbors' in r and r['bgp_neighbors']:#on vérifie si le routeur a des voisins bgp dans json
+            bgp_neighbors = r['bgp_neighbors']#on recup la liste
+            asn = r['asn']#et le num de l'as
+            lines.append(f"router bgp {asn}")#on rentre dans la conf bgp
             
             if 'router_id' in r:
                 lines.append(f" bgp router-id {r['router_id']}")
@@ -115,6 +115,7 @@ def genere_config():
 
 if __name__=='__main__':
     genere_config()
+
 
 
 
