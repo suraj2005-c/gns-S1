6f79564983d5075230287a49c3b18d3068868714
AriHazlol <46625597+AriHazlol@users.noreply.github.com>
2026-01-22 10:41:15 +0100

    Update 3'config.py

--- 3'config.py
+++ 3'config.py
@@ -1,11 +1,66 @@
 import json
 import os #créer des dossiers et manipuler les chemins de fichiers
 import re #recherches rapide
+import getpass
+import platform
 
-
-project_path = input("Entrez le chemin complet du projet : ")
 intent_file='intent_file.json' #fichier source
 
+def check_system():
+    version = platform.release().lower()
+    if "microsoft" in version:
+        return "wsl"
+    if platform.system() == "Windows":
+        return "windows_native"
+    return "linux_native"
+
+
+def get_path():
+    ask_path = input("Voulez-vous entrer le chemin vers votre projet GNS3 manuellement ? (y/n) ")
+
+    if ask_path == 'y':
+        while True:
+            project_path = input("Veuillez entrer le chemin vers votre projet : ")
+            if os.path.exists(project_path):
+                break
+            print("Erreur : Le chemin spécifié n'existe pas. Réessayez.")
+    else:
+        project_name = input("Entrez le nom du projet : ")
+        user = getpass.getuser()
+        sys_type = check_system()
+        project_path = None
+
+        if sys_type == "wsl":
+            win_user = input(f"Nom d'utilisateur Windows (par défaut '{user}') : ") or user
+            paths_to_check = [
+                f"/mnt/c/Users/{win_user}/GNS3/projects/{project_name}",
+                f"/mnt/d/Users/{win_user}/GNS3/projects/{project_name}"
+            ]
+        elif sys_type == "windows_native":
+            paths_to_check = [
+                f"C:/Users/{user}/GNS3/projects/{project_name}",
+                f"D:/Users/{user}/GNS3/projects/{project_name}"
+            ]
+        else:
+            paths_to_check = [f"/home/{user}/GNS3/projects/{project_name}"]
+
+        for path in paths_to_check:
+            if os.path.exists(path):
+                project_path = path
+                print("Project Path Valide ! ")
+                break
+        
+        if not project_path:
+            print("Chemin introuvable, veuillez relancer et/ou entrer le chemin manuellement.")
+
+    return project_path
+
+def trouver_fichier_config(dossier_router, nom_fichier):
+    for racines, dirs, files in os.walk(dossier_router):
+        if nom_fichier in files:
+            return os.path.join(racines, nom_fichier)
+        
+project_path = get_path()
 def trouver_fichier_config(dossier_router, nom_fichier):
     for racines, dirs, files in os.walk(dossier_router):
         if nom_fichier in files:
